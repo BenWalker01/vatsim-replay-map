8c8a7796ad11463f5e93640864fe8020a0ec3244
Ben Walker <146777065+BenWalker01@users.noreply.github.com>
2024-08-25 10:09:09 +0100

    working replay system

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
 import './App.css';
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState, useCallback, useRef } from 'react';
 import { Layer, polyline, Map } from 'leaflet';
 import MapComponent from './MapComponent';
 import FileUploader from './FileUploader';
 import { douglasPeucker, getColourByAlt } from './utils';
 import ReactSlider from "react-slider";
 import debounce from "lodash/debounce"
+import L from "leaflet"
+import planeSvg from "./plane.svg"
+
 
 const App: React.FC = () => {
     const [fileData, setFileData] = useState<{ [fileName: string]: any }>({});
     const [fileVisibility, setFileVisibility] = useState<{ [fileName: string]: boolean }>({});
     const [drawnLayers, setDrawnLayers] = useState<{ [fileName: string]: Layer[] }>({});
     const [leafletMap, setLeafletMap] = useState<Map | null>(null);
-    const [sliderValues, setSliderValues] = useState<[number, number]>([0, 100])
+    const [sliderValues, setSliderValues] = useState<[number, number]>([0, 100]);
+    const [intervalId, setIntervalId] = useState<NodeJS.Timeout | null>(null);
+    const [isLogging, setIsLogging] = useState(false);
+    const [counter, setCounter] = useState(0);
+    const [isPaused, setIsPaused] = useState(false);
+    const [pauseStartTime, setPauseStartTime] = useState<Date | null>(null);
+    const [remainingTime, setRemainingTime] = useState(0);
+    const [coords, setCoords] = useState<{ [key: string]: { lat: number, lng: number, alt: number, offset: number }[] }>({});
+    const [speed, setSpeed] = useState(1);
+    const markersRef = useRef<Array<[string, L.Marker<any>]>>([]);
+    const circlesRef = useRef<Array<[string, L.Circle<any>]>>([]);
+    const prevCoordsRef = useRef<{ [key: string]: { lat: number, lng: number } }>({});
 
-    const drawLines = useCallback((callsignData: { [key: string]: { lat: number, lng: number, alt: number, timestamp: number }[] }, fileName: string) => {
+
+    const drawLines = useCallback((callsignData: { [key: string]: { lat: number, lng: number, alt: number, offset: number }[] }, fileName: string) => {
         if (!leafletMap) return;
 
         setDrawnLayers(prevLayers => {
@@ -26,24 +41,29 @@ const App: React.FC = () => {
 
         if (!fileVisibility[fileName]) return;
 
+
         const newLayers: Layer[] = [];
         Object.keys(callsignData).forEach(callsign => {
-            const coords = callsignData[callsign].map(({ lat, lng, alt, timestamp }) => ({ lat, lng, alt, timestamp }));
+            const coords = callsignData[callsign].map(({ lat, lng, alt, offset }) => ({ lat, lng, alt, offset }));
+            setCoords(prevCoords => ({ ...prevCoords, [callsign]: coords }));
             const simplifiedCoords = douglasPeucker(coords, 0.001); // Adjust epsilon as needed
+
+
             for (let i = 0; i < simplifiedCoords.length - 1; i++) {
                 const { lat: lat1, lng: lng1, alt: alt1 } = simplifiedCoords[i];
                 const { lat: lat2, lng: lng2, alt: alt2 } = simplifiedCoords[i + 1];
                 const avgAlt = (alt1 + alt2) / 2;
-                if (avgAlt >= (sliderValues[0] / 100) * 40000 && avgAlt <= (sliderValues[1] / 100) * 40000) {
-                    const color = getColourByAlt(avgAlt);
-                    const line = polyline([[lat1, lng1], [lat2, lng2]], { color, weight: 2 }).addTo(leafletMap);
-                    newLayers.push(line);
-                }
+                const color = getColourByAlt(avgAlt);
+                const line = polyline([[lat1, lng1], [lat2, lng2]], { color });
+                line.addTo(leafletMap);
+                newLayers.push(line);
             }
+
+
         });
 
         setDrawnLayers(prevLayers => ({ ...prevLayers, [fileName]: newLayers }));
-    }, [leafletMap, fileVisibility, sliderValues]);
+    }, [leafletMap, fileVisibility, setDrawnLayers]);
 
     const handleFileParsed = (data: { [fileName: string]: any }) => {
         setFileData(prevData => ({ ...prevData, ...data }));
@@ -60,6 +80,146 @@ const App: React.FC = () => {
             [fileName]: !prevVisibility[fileName]
         }));
     };
+
+    const createSvgMarker = (lat: number, lng: number) => {
+        const icon = L.divIcon({
+            html: `<img src="${planeSvg}" style="width: 24px; height: 24px; filter: hue-rotate(90deg) saturate(1000%);" />`,
+            className: '',
+            iconSize: [24, 24],
+        });
+        return L.marker([lat, lng], { icon });
+    };
+
+    const handlePlayClick = () => {
+        if (!isLogging) {
+            const intervalDuration = 1000 / speed;
+            const id = setInterval(() => {
+                setCounter(prevCounter => prevCounter + 1);
+            }, intervalDuration);
+            setIntervalId(id);
+            setIsLogging(true);
+            setIsPaused(false);
+        }
+    };
+
+    const handlePauseClick = () => {
+        if (isPaused) {
+            // Resume logging
+            if (pauseStartTime) {
+                const currentTime = new Date();
+                const pausedDuration = currentTime.getTime() - pauseStartTime.getTime();
+                const newRemainingTime = remainingTime - pausedDuration;
+
+                setTimeout(() => {
+                    const intervalDuration = 1000 / speed;
+                    const id = setInterval(() => {
+                        setCounter(prevCounter => prevCounter + 1);
+                    }, intervalDuration);
+                    setIntervalId(id);
+                }, newRemainingTime);
+
+                setIsPaused(false);
+                setPauseStartTime(null);
+            }
+        } else {
+            // Pause logging
+            if (intervalId) {
+                clearInterval(intervalId);
+                setIntervalId(null);
+                setIsPaused(true);
+                setPauseStartTime(new Date());
+                const currentTime = new Date();
+                const elapsedTime = currentTime.getTime() % (1000 / speed);
+                setRemainingTime((1000 / speed) - elapsedTime);
+            }
+        }
+    };
+    const drawGreenCircle = (lat: number, lng: number) => {
+        return L.circle([lat, lng], {
+            color: 'green',
+            fillColor: 'green',
+            fillOpacity: 0.5,
+            radius: 1000
+        });
+    };
+
+    const handleIncreaseSpeed = () => {
+        setSpeed(prevSpeed => prevSpeed + 1)
+    }
+    const handleDecreaseSpeed = () => {
+        setSpeed(prevSpeed => (prevSpeed > 1 ? prevSpeed - 1 : 1)); // Ensure speed doesn't go below 1
+    };
+
+    useEffect(() => {
+        return () => {
+            if (intervalId) {
+                clearInterval(intervalId);
+            }
+        };
+    }, [intervalId]);
+
+    useEffect(() => {
+        const newMarkersBuffer: Array<[string, L.Marker<any>]> = [];
+        const newCirclesBuffer: Array<[string, L.Circle<any>]> = [];
+        const movedCallsigns: Set<string> = new Set();
+
+        Object.keys(coords).forEach(callsign => {
+            const callsignCoords = coords[callsign];
+            const matchingCoord = callsignCoords.find(coord => coord.offset === counter);
+            if (matchingCoord) {
+                const prevCoord = prevCoordsRef.current[callsign];
+                if (!prevCoord || prevCoord.lat !== matchingCoord.lat || prevCoord.lng !== matchingCoord.lng) {
+                    console.log(`Adding marker for callsign: ${callsign} at Lat: ${matchingCoord.lat}, Lng: ${matchingCoord.lng}`);
+
+                    movedCallsigns.add(callsign);
+                    const marker = createSvgMarker(matchingCoord.lat, matchingCoord.lng);
+                    newMarkersBuffer.push([callsign, marker]);
+
+                    const circle = drawGreenCircle(matchingCoord.lat, matchingCoord.lng);
+                    newCirclesBuffer.push([callsign, circle]);
+
+                    prevCoordsRef.current[callsign] = { lat: matchingCoord.lat, lng: matchingCoord.lng };
+
+                    console.log("Added plane: ", callsign);
+                }
+            }
+        });
+
+        if (movedCallsigns.size > 0) {
+            console.log("Drawn new planes. Removing old planes");
+
+            // Remove only the markers and circles for the moved callsigns
+            markersRef.current = markersRef.current.filter(marker => {
+                if (movedCallsigns.has(marker[0])) {
+                    marker[1].remove();
+                    return false;
+                }
+                return true;
+            });
+
+            circlesRef.current = circlesRef.current.filter(circle => {
+                if (movedCallsigns.has(circle[0])) {
+                    circle[1].remove();
+                    return false;
+                }
+                return true;
+            });
+
+            // Add new markers and circles from buffer to the main arrays
+            if (leafletMap) {
+                newMarkersBuffer.forEach(marker => {
+                    marker[1].addTo(leafletMap);
+                    markersRef.current.push(marker);
+                });
+                newCirclesBuffer.forEach(circle => {
+                    circle[1].addTo(leafletMap);
+                    circlesRef.current.push(circle);
+                });
+            }
+        }
+    }, [counter, coords, leafletMap]);
+
+
 
     useEffect(() => {
         Object.keys(fileData).forEach(fileName => {
@@ -99,9 +259,13 @@ const App: React.FC = () => {
             />
             <footer className="toolbar">
                 <div className="replay-system">
-                    <label className="wip-label">WIP: Replay system</label>
-                    <button>Play</button>
-                    <button>Pause</button>
+                    <button onClick={handlePlayClick}>Play</button>
+                    <button onClick={handlePauseClick}>{isPaused ? 'Resume' : 'Pause'}</button>
+                    <div className="speed-controls">
+                        <button onClick={handleDecreaseSpeed}>Decrease Speed</button>
+                        <span className="speed-text">{speed}x</span>
+                        <button onClick={handleIncreaseSpeed}>Increase Speed</button>
+                    </div>
                 </div>
                 <div className="slider-container">
                     <div className="slider-values">
--- src/FileUploader.tsx
+++ src/FileUploader.tsx
@@ -31,24 +31,42 @@ const FileUploader: React.FC<FileUploaderProps> = ({ onFilesParsed }) => {
 
     const parseFileData = (data: string) => {
         const lines = data.split("\n");
-        const callsignData: { [key: string]: { lat: number, lng: number, alt: number, timestamp: number }[] } = {};
-        lines.forEach(line => {
+        const callsignData: { [key: string]: { lat: number, lng: number, alt: number, offset: number }[] } = {};
+        let startTimeInSeconds;
+        // if (lines[0] === '\r') {
+        //     lineOne = lines[1];
+        // }
+        // else { lineOne = lines[0]; }
+        // const startTime = lineOne.slice(1, -1).split(" ")[0];
+        // const [startHours, startMinutes, startSeconds] = startTime.split(":").map(Number);
+        // const startTimeInSeconds = startHours * 3600 + startMinutes * 60 + startSeconds;
+        for (let i = 0; i < lines.length - 1; i++) {
+            const line = lines[i]
             if (line.startsWith("@N:")) {
+                if (!startTimeInSeconds) {
+                    const startTime = lines[i - 1].slice(1, -1).split(" ")[0];
+                    const [startHours, startMinutes, startSeconds] = startTime.split(":").map(Number);
+                    startTimeInSeconds = startHours * 3600 + startMinutes * 60 + startSeconds;
+                }
                 const parts = line.split(":");
                 if (parts.length >= 8) {
                     const callsign = parts[1];
                     const lat = parseFloat(parts[4]);
                     const lng = parseFloat(parts[5]);
                     const alt = parseFloat(parts[6]);
-                    const timestamp = parseFloat(parts[7]);
-
+                    const timeLine = lines[i - 1]
+                    const time = timeLine.slice(1, -1).split(" ")[0]
+                    const [hours, minutes, seconds] = time.split(":").map(Number);
+                    const timeInSeconds = hours * 3600 + minutes * 60 + seconds;
+                    const offset = timeInSeconds - startTimeInSeconds
                     if (!callsignData[callsign]) {
                         callsignData[callsign] = [];
                     }
-                    callsignData[callsign].push({ lat, lng, alt, timestamp });
+                    //console.log("offset: ", offset)
+                    callsignData[callsign].push({ lat, lng, alt, offset });
                 }
             }
-        });
+        };
         return callsignData;
     };
 
